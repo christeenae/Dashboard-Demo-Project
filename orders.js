90ff6d5ed5f6f4ebb9ac29a14809b317103e3549
Christeena Edison <89634297+christeenae@users.noreply.github.com>
2023-10-01 13:00:22 +0530

    uploading html,css and js files

---
+++ orders.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: 'Movie Webpage',
+        productNumber: '003',
+        status: 'Active'
+    },
+    {
+        productName: 'Smart Traffic system ',
+        productNumber: '002',
+        status: 'Not Active'
+    },
+    {
+        productName: 'Car Sales Webpage',
+        productNumber: '004',
+        status: 'Active'
+    },
+    {
+        productName: 'Sorting Simulator',
+        productNumber: '001',
+        status: 'Pending'
+    },
+]
